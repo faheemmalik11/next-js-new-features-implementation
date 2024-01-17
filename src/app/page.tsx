fb9c1b90e2060a4f0f14ba28d905de19de247f86
faheemmalik11 <faheemmalik3000@gmail.com>
2024-01-17 18:44:22 +0500

    implementation on hydration issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+import dynamic from "next/dynamic";
+import { useState, useEffect } from "react";
+
+// import HydrationTest from "@/components/hydration"
+const HydrationTestNoSSR = dynamic(() => import("../components/hydration"), {
+  ssr: false,
+});
 
 export default function Home() {
+  const a = Math.random();
+  console.log(a);
   return (
-    <div>Home Page</div>
-  )
+    <div>
+      Home Page <HydrationTestNoSSR />
+      <div suppressHydrationWarning>{a}</div>
+    </div>
+  );
 }
